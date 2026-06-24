ced8ce0b999ee123e7b55063d6f601da418718c4
Matt Travi <programmer@travi.org>
2026-06-23 22:41:39 -0500

    test(unit): switch from jest-when to vitest-when

--- src/lift.test.js
+++ src/lift.test.js
@@ -6,7 +6,7 @@ import badgePlugin from '@form8ion/remark-inject-badges';
 
 import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
 import any from '@travi/any';
-import {when} from 'jest-when';
+import {when} from 'vitest-when';
 
 import remarkConfig from '../.remarkrc.mjs';
 import lift from './lift.js';
@@ -29,9 +29,9 @@ describe('lift', () => {
     process = vi.fn();
 
     remark.mockReturnValue({data});
-    when(data).calledWith('settings', remarkConfig.settings).mockReturnValue({use});
-    when(use).calledWith(legacyMarkerPlugin).mockReturnValue({use});
-    when(use).calledWith(badgePlugin, badges).mockReturnValue({use});
+    when(data).calledWith('settings', remarkConfig.settings).thenReturn({use});
+    when(use).calledWith(legacyMarkerPlugin).thenReturn({use});
+    when(use).calledWith(badgePlugin, badges).thenReturn({use});
   });
 
   afterEach(() => {
@@ -39,9 +39,9 @@ describe('lift', () => {
   });
 
   it('should update the readme', async () => {
-    when(use).calledWith(readmePlugin, documentation).mockReturnValue({process});
-    when(fs.readFile).calledWith(pathToReadmeFile, 'utf8').mockResolvedValue(existingFileContents);
-    when(process).calledWith(existingFileContents).mockReturnValue(updatedFileContents);
+    when(use).calledWith(readmePlugin, documentation).thenReturn({process});
+    when(fs.readFile).calledWith(pathToReadmeFile, 'utf8').thenResolve(existingFileContents);
+    when(process).calledWith(existingFileContents).thenReturn(updatedFileContents);
 
     await lift({projectRoot, results: {badges, documentation}});
 
@@ -49,9 +49,9 @@ describe('lift', () => {
   });
 
   it('should default `documentation` content to an empty object', async () => {
-    when(use).calledWith(readmePlugin, {}).mockReturnValue({process});
-    when(fs.readFile).calledWith(pathToReadmeFile, 'utf8').mockResolvedValue(existingFileContents);
-    when(process).calledWith(existingFileContents).mockReturnValue(updatedFileContents);
+    when(use).calledWith(readmePlugin, {}).thenReturn({process});
+    when(fs.readFile).calledWith(pathToReadmeFile, 'utf8').thenResolve(existingFileContents);
+    when(process).calledWith(existingFileContents).thenReturn(updatedFileContents);
 
     await lift({projectRoot, results: {badges}});
 
@@ -60,11 +60,11 @@ describe('lift', () => {
 
   it('should reject the promise when a processing error occurs', async () => {
     const error = new Error('from test');
-    when(use).calledWith(readmePlugin, documentation).mockReturnValue({process});
+    when(use).calledWith(readmePlugin, documentation).thenReturn({process});
     process.mockImplementation(() => {
       throw error;
     });
 
-    expect(() => lift({results: {badges, documentation}})).rejects.toThrowError(error);
+    await expect(() => lift({results: {badges, documentation}})).rejects.toThrow(error);
   });
 });
